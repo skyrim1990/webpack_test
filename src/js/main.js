aaa7b284875315d87501372422f654a70cffa4f6
skyrim <skyrim1990@163.com>
2017-02-09 16:51:15 +0800

    js HMR

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import * as A from './lib/a';
 import * as B from './lib/b';
 
+//require('./index.html')
+
 A.log(B.b);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: './src/js/main.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist/js')
+	},
+	plugins:[
+		/*
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+				warnings: false,	
+			},
+		}),	
+		*/
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'	
+		})
+	],
+	module: {
+		loaders: [{
+			test: /\.css$/,
+			loaders: ['style', 'css']
+		},{
+			test: /\.html$/,
+			loader: "raw-loader"
+		}]	
+	},
+	devServer: {
+		contentBase: './dist',
+		hot: true
 	}
 };
